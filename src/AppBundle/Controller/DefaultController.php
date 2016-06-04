9987653b05d5c81085e7303ef70ba02d9199b579
jean.angelis <jean.angelis@gmail.com>
2016-06-04 13:45:37 -0300

    Added assetic bundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Process\Process;
 
 class DefaultController extends Controller
 {
@@ -14,6 +16,20 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return new JsonResponse();
+        return $this->render(":default:index.html.twig");
+    }
+
+    /**
+     * @Route("/load", name="load")
+     */
+    public function loadUrls(Request $request)
+    {
+        $urlsString = explode("\n", $request->get('urls'));
+
+        foreach ($urlsString as $url) {
+            
+        }
+
+        return new RedirectResponse($this->generateUrl('homepage'));
     }
 }
