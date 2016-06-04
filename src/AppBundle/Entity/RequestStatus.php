b500b2a0362bdae35799cd3ef0e09f04f2d4c712
jean.angelis <jean.angelis@gmail.com>
2016-06-04 17:39:05 -0300

    Url and RequestStatus entities created

---
+++ src/AppBundle/Entity/RequestStatus.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RequestStatus
+ *
+ * @ORM\Table(name="request_status")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RequestStatusRepository")
+ */
+class RequestStatus
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, unique=true)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return RequestStatus
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
---
+++ src/AppBundle/Entity/Url.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Url
+ *
+ * @ORM\Table(name="url")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UrlRepository")
+ */
+class Url
+{
+    /**
+     * Times the URL can be redirected
+     */
+    const MAX_REDIRECTS = 5;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="batch", type="integer")
+     */
+    private $batch;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\RequestStatus")
+     */
+    private $status;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Url
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set batch
+     *
+     * @param integer $batch
+     *
+     * @return Url
+     */
+    public function setBatch($batch)
+    {
+        $this->batch = $batch;
+
+        return $this;
+    }
+
+    /**
+     * Get batch
+     *
+     * @return int
+     */
+    public function getBatch()
+    {
+        return $this->batch;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return Url
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
+
