af541d1437cf30625d5dcf8b97a54c5ac2fffb07
jean.angelis <jean.angelis@gmail.com>
2016-06-04 17:55:51 -0300

    RequestStatusFixture created

---
+++ src/AppBundle/DataFixtures/ORM/LoadRequestStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\RequestStatus;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class LoadRequestStatus
+ */
+class LoadRequestStatus implements FixtureInterface
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $okStatus = new RequestStatus();
+    }
+}
